4b2154c2be797d86ee4937ed3151fcc4d603b827
dengzhiteng <dengzhiteng@jwx.com.cn>
2021-01-07 16:01:41 +0800

    计算属性

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo"  src="../assets/logo.png">
+    <h1>count: {{ count }}</h1>
+    <h1>doubleCount: {{ doubleCount }}</h1>
+    <button @click="add">add</button>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
-@Options({
-  components: {
+<script>
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    const count = ref(222);
+    const add = () => {
+      count.value++;
+    };
+    const doubleCount = computed(() => count.value * 2);
+    return {
+      count,
+      add,
+      doubleCount,
+    };
   },
-})
-export default class Home extends Vue {}
+};
 </script>
